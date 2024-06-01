81113c27a24853debc5d337542f3e2d52c53370e
bt <btfranzen@gmail.com>
2024-05-31 20:10:15 -0600

    1431 && 605

--- leetcode.js
+++ leetcode.js
@@ -68,4 +68,35 @@ Note that multiple kids can have the greatest number of candies.
  * @param {number} extraCandies
  * @return {boolean[]}
  */
-const kidsWithCandies = function (candies, extraCandies) {};
+const kidsWithCandies = function (candies, extraCandies) {
+    const maxCandies = Math.max(...candies);
+
+    return candies.map((candy) => candy + extraCandies >= maxCandies);
+};
+
+/**
+ * 605. Can Place Flowers
+ * You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots. Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+ */
+
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+const canPlaceFlowers = function(flowerbed, n) {
+    let count = 0;
+
+    for(let i=0 ; i<flowerbed.length ; i++) {
+        if (flowerbed[i] === 0) {
+            const prev = flowerbed[i - 1] || 0;
+            const next = flowerbed[i + 1] || 0;
+
+            if (prev === 0  && next === 0) {
+                flowerbed[i] = 1;
+                count ++;
+            }
+        }
+    }
+
+    return count >= n;
