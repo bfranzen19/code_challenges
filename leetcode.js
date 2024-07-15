6debe1ebb6620e035c1e184197ac0a712603cb00
bt <btfranzen@gmail.com>
2024-07-14 21:38:52 -0600

    443

--- leetcode.js
+++ leetcode.js
@@ -130,5 +130,36 @@ const reverseVowels = (s) => {
 const reverseWords = (s) => s.split(' ').filter(el => el.length > 0).reverse().join(' ');
 
 /**
- * 
+ * 443. String Compression
+ * Given an array of characters chars, compress it using the following algorithm:
+ * Begin with an empty string s. For each group of consecutive repeating characters in chars:
+ * If the group's length is 1, append the character to s. 
+ * Otherwise, append the character followed by the group's length.
+ * The compressed string s should not be returned separately, but instead, be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars. After you are done modifying the input array, return the new length of the array. You must write an algorithm that uses only constant extra space.
  */
+
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+const compress = (chars) => {
+    let count = 0, current = chars[0];
+    chars.push("DONE");
+
+    while (current!=="DONE") {
+        if (current === chars[0]) {
+            ++count;
+            chars.shift();
+        }
+        else {
+            chars.push(current);
+            if (count > 1) {
+                chars.push(...(count+""));
+            }
+            count = 0;
+            current = chars[0];
+        }
+    }
+    chars.shift();
+    return chars.length;
+};
